fdabb5d78d14fd799d9085689009c200ce0aea96
TiagoBaratieri <tiagobaratieri97@gmail.com>
2021-05-05 13:01:00 -0300

    Reply

--- backend/src/main/java/com/baratieri/dslearn/entities/Topic.java
+++ backend/src/main/java/com/baratieri/dslearn/entities/Topic.java
@@ -35,6 +35,10 @@ public class Topic implements Serializable {
     @JoinColumn(name = "lesson_id")
     private Lesson lesson;
 
+    @ManyToOne
+    @JoinColumn(name = "reply_id")
+    private Reply answer;
+
     @ManyToMany
     @JoinTable(name = "tb_topic_likes",
             joinColumns = @JoinColumn(name = "topic_id"),
@@ -113,6 +117,14 @@ public class Topic implements Serializable {
 
     public Set<User> getLikes() {
         return likes;
+    }
+
+    public Reply getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(Reply answer) {
+        this.answer = answer;
     }
 
     @Override
